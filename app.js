9819b1b0fc91369b20c639c5578fa86768fef5d4
unknown <jluisviegas@hotmail.com>
2022-09-06 19:46:20 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+let currentInput = document.querySelector('.current-input');
+let answerScreen = document.querySelector('.answer-screen');
+let buttons = document.querySelectorAll('button');
+let erasebtn = document.querySelector('#erase');
+let clearbtn = document.querySelector('#clear');
+let evaluate = document.querySelector('#evaluate');
+let realTimeScreenValue = [];
+
+clearbtn.addEventListener('click', () => {
+	realTimeScreenValue = [''];
+	answerScreen.innerHTML = 0;
+	currentInput.className = 'current-input';
+	answerScreen.className = 'answer-screen';
+	answerScreen.style.color = 'rgba(150, 150, 150, 0.87)';
+});
+
+buttons.forEach((btn) => {
+	btn.addEventListener('click', () => {
+		if (!btn.id.match('erase')) {
+			realTimeScreenValue.push(btn.value);
+			currentInput.innerHTML = realTimeScreenValue.join('');
+			if (btn.classList.contains('num-btn')) {
+				answerScreen.innerHTML = eval(realTimeScreenValue.join(''));
+			}
+		}
+		if (btn.id.match('erase')) {
+			realTimeScreenValue.pop();
+			currentInput.innerHTML = realTimeScreenValue.join('');
+			answerScreen.innerHTML = eval(realTimeScreenValue.join(''));
+		}
+		if (btn.id.match('evaluate')) {
+			currentInput.className = 'asnwer-screen';
+			answerScreen.className = 'current-input';
+			asnwerScreen.style.color = 'white';
+		}
+		if (typeof eval(realTimeScreenValue.join('')) == 'undefined') {
+			answerScreen.innerHTML = 0;
+		}
+	});
+});
